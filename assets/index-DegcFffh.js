79310dda7dcfdf89b9bcbac7bb99d151d1153105
luyh7 <31462127+luyh7@users.noreply.github.com>
2025-01-23 11:16:23 +0000

    Deploying to gh-pages from  @ 9b658cc4cc1dc90d09103a993235704ffd8c23fa 🚀

---
+++ assets/index-DegcFffh.js
@@ -0,0 +1,1 @@
+import{d as t,av as r,aw as a,o,c as s}from"./vue-BqKPdOxY.js";const m=t({__name:"index",setup(n){const e=r();return a().replace({path:`/${e.params.path}`,query:e.query}),(c,p)=>(o(),s("div"))}});export{m as default};
