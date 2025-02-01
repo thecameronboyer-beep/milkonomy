49a434d0e22b5720fb34878121b4188567f5a31b
luyh7 <31462127+luyh7@users.noreply.github.com>
2025-02-01 14:57:42 +0000

    Deploying to gh-pages from  @ 9c18af9576834d1ea691c257b54b39cd504f17af 🚀

---
+++ assets/index-CSznDpR7.js
@@ -0,0 +1,1 @@
+import{d as t,av as r,aw as a,c as o,o as s}from"./vue-Cz8nu7m0.js";const m=t({__name:"index",setup(n){const e=r();return a().replace({path:`/${e.params.path}`,query:e.query}),(c,p)=>(s(),o("div"))}});export{m as default};
