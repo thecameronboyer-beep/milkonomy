4b1d26c5ae87eab58900177bce78a87517e11df2
luyh7 <31462127+luyh7@users.noreply.github.com>
2025-02-22 06:52:11 +0000

    Deploying to gh-pages from  @ 0cf4b5eacd400aa06967caa5a629cb20dae09f36 🚀

---
+++ assets/use-fullscreen-loading-CCS3fw67.js
@@ -0,0 +1,10 @@
+import"./index-CyT5pB4f.js";/* empty css                  *//* empty css                */import{ac as m,o as _,u as g,b as u}from"./element-KbGxPU8k.js";import{d as f,c as y,o as E,L as a,J as n,a2 as i}from"./vue-BKoFBWM3.js";import{_ as w}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./vxe-BEJQedsn.js";const x={lock:!0,text:"加载中..."},p=(o,e={})=>{let r;return async(...c)=>{try{return r=m.service({...x,...e}),await o(...c)}finally{r.close()}}},C={code:0,data:{list:[]},message:"获取成功"};function S(o){return new Promise(e=>{setTimeout(()=>{e({...C,data:{list:o}})},1e3)})}function v(){return new Promise((o,e)=>{setTimeout(()=>{e(new Error("发生错误"))},1e3)})}const k={class:"app-container"},A=`
+  <path class="path" d="
+    M 30 15
+    L 28 17
+    M 25.61 25.61
+    A 15 15, 0, 0, 1, 15 30
+    A 15 15, 0, 1, 1, 27.99 7.5
+    L 15 15
+  " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+`,L=f({__name:"use-fullscreen-loading",setup(o){const e={text:"即将发生错误...",background:"#F56C6C20",svg:A,svgViewBox:"-10, -10, 50, 50"};async function r(){const s=await p(S)([1,2,3]);u.success(`${s.message}，传参为 ${s.data.list.toString()}`)}async function c(){try{await p(v,e)()}catch(s){u.error(s.message)}}return(s,t)=>{const l=_,d=g;return E(),y("div",k,[a(l,{shadow:"never"},{default:n(()=>t[0]||(t[0]=[i(" 该示例是演示：通过将要执行的函数传递给 composable，让 composable 自动开启全屏 loading，函数执行结束后自动关闭 loading ")])),_:1}),a(l,{header:"示例",shadow:"never"},{default:n(()=>[a(d,{type:"primary",onClick:r},{default:n(()=>t[1]||(t[1]=[i(" 查询成功 ")])),_:1}),a(d,{type:"danger",onClick:c},{default:n(()=>t[2]||(t[2]=[i(" 查询失败 ")])),_:1})]),_:1})])}}}),I=w(L,[["__scopeId","data-v-d330363c"]]);export{I as default};
